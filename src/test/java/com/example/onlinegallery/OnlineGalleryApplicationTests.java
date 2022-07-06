31c1fa2828f365d92eda8aea011a1f23706d1a97
Marianoguerrero <mariano.guerrero@tsoftlatam.com>
2022-07-06 09:15:56 -0300

    first

---
+++ src/test/java/com/example/onlinegallery/OnlineGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinegallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
